b6f8ae2634787151d64c26d07a97e6dc18b10c28
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-09-08 09:19:35 +0200

    // Version updated to v1.2.4

--- sekeywords.php
+++ sekeywords.php
@@ -37,7 +37,7 @@ class SEKeywords extends ModuleGraph
 	{
 		$this->name = 'sekeywords';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.2.3';
+		$this->version = '1.2.4';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
